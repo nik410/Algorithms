773646d6fcc4e48c4f335019283d2811b7c141b6
Niket Pandey <107863847+nik410@users.noreply.github.com>
2024-02-01 14:31:06 +0530

    Update 46. Permutations.java

--- Recursion/Medium/46. Permutations.java
+++ Recursion/Medium/46. Permutations.java
@@ -1,22 +1,81 @@
 class Solution {
 
 
-    public void permut(int nums[], List<List<Integer>> ans, List<Integer> al,int visited[]){
-        if(al.size() == nums.length){
-            ans.add(new ArrayList(al));
-            return;
+    // public void permut(int nums[], List<List<Integer>> ans, List<Integer> al,int visited[]){
+    //     if(al.size() == nums.length){
+    //         ans.add(new ArrayList(al));
+    //         return;
+    //     }
+
+    //     for(int i = 0; i < visited.length; i++){
+    //         if(visited[i] == 0){
+    //             visited[i] = 1;
+    //             al.add(nums[i]);
+    //             permut(nums, ans, al, visited);
+    //             visited[i] = 0;
+    //             al.remove(al.size() - 1);
+    //         }
+    //     }
+    // }
+
+
+    class Solution {
+
+
+    // public void permut(int nums[], List<List<Integer>> ans, List<Integer> al,int visited[]){
+    //     if(al.size() == nums.length){
+    //         ans.add(new ArrayList(al));
+    //         return;
+    //     }
+
+    //     for(int i = 0; i < visited.length; i++){
+    //         if(visited[i] == 0){
+    //             visited[i] = 1;
+    //             al.add(nums[i]);
+    //             permut(nums, ans, al, visited);
+    //             visited[i] = 0;
+    //             al.remove(al.size() - 1);
+    //         }
+    //     }
+    // }
+
+    public void swap(int a, int b){
+      
+        return;
+    }
+
+
+     //Better Solution
+        
+    public void permut(int nums[], int index, List<List<Integer>> ans){
+        if(index == nums.length){
+            ans.add(new ArrayList(Arrays.stream(nums).boxed().toList()));
+            return ;
         }
 
-        for(int i = 0; i < visited.length; i++){
-            if(visited[i] == 0){
-                visited[i] = 1;
-                al.add(nums[i]);
-                permut(nums, ans, al, visited);
-                visited[i] = 0;
-                al.remove(al.size() - 1);
-            }
+        for(int i = index; i < nums.length; i++){
+            
+            //Swapping
+             int temp = nums[index];
+             nums[index] = nums[i];
+             nums[i] = temp;
+
+            permut(nums, index + 1, ans);
+
+            //Swapping
+            temp = nums[index];
+             nums[index] = nums[i];
+             nums[i] = temp;
+
         }
     }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        
+        permut(nums, 0, ans);
+        return ans;
+    }
+}
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList();
         List<Integer> al = new ArrayList();
