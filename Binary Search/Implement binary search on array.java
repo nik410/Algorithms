3ca4ca0742d8480eee5bb924e58f3439c0238369
Niket Pandey <107863847+nik410@users.noreply.github.com>
2024-02-27 16:05:26 +0530

    Create Implement binary search on array.java

---
+++ Binary Search/Implement binary search on array.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public static int search(int []nums, int target) {
+        int ans = -1;
+
+        int l = 0, r = nums.length - 1;
+
+        while( l <= r){
+            int mid = l + (r - l)/ 2;
+            
+            if(nums[mid] == target)return mid;
+            else if(nums[mid] < target) l = mid + 1;
+            else{
+                r = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
