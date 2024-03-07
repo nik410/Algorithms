769d00a78020ce757a72b0864cc314f4f952964f
Niket Pandey <107863847+nik410@users.noreply.github.com>
2024-03-07 17:09:01 +0530

    Create Insert in middle of linked list.java

---
+++ Linked List/Insert in middle of linked list.java
@@ -0,0 +1,29 @@
+/*
+Structure of node class is:
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    
+    public Node insertInMid(Node head, int data){
+        Node slow = head;
+        Node fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;;
+        }
+        Node inser = new Node(data);
+        inser.next = slow.next; 
+        slow.next = inser;
+        
+        return head;
+        
+    }
+}
