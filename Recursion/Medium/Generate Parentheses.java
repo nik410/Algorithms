647bba4c78aea9260fdc5f36270b53fd7da1790e
Niket Pandey <107863847+nik410@users.noreply.github.com>
2024-03-12 23:14:57 +0530

    Update Generate Parentheses.java

--- Recursion/Medium/Generate Parentheses.java
+++ Recursion/Medium/Generate Parentheses.java
@@ -1,25 +1,45 @@
-public static void gsubsequences(int n, int count1, int count2, ArrayList<StringBuilder> al,StringBuilder stb){
+class Solution {
+    
+    public static void ways(int n, List<String> ans, StringBuilder stb, int count1, int count2){
         
-        
-        if(count1 + count2 == 2*n){
-                
-                al.add(new StringBuilder(stb));
-            
+        if(stb.length() == 2 * n){
+            if(count1 == count2){
+                ans.add(stb.toString());
+                stb = new StringBuilder();
+            }            
             return;
         }
-        if(count1 < n ){
-            stb.append('(');
-            gsubsequences(n, count1 + 1, count2, al, stb);
-            stb.delete(stb.length() - 1, stb.length());
-        }
-         
-        if(count2 < count1 ){
-            stb.append(')');
-            gsubsequences(n, count1, count2 + 1, al, stb);
-            stb.delete(stb.length() - 1, stb.length());
-        }
+
+            if(count1 < n){
+              stb.append("(");
+                count1 += 1;
+                ways(n, ans, stb, count1, count2);
+                if(stb.length() > 0){
+                    stb.deleteCharAt(stb.length() - 1);
+                    count1 -= 1;
+                }
+             }   
+
+
+                if(count1 > count2 && count2 < n){
+                stb.append(")");
+                count2 += 1;
+                ways(n, ans, stb, count1, count2);
+                if(stb.length() > 0){
+                    stb.deleteCharAt(stb.length() - 1);
+                    count2 -= 1;
+                    }
+                }    
         
-        
-       
+
+
     }
-    
+
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        StringBuilder stb = new StringBuilder();
+        ways(n, ans, stb, 0 , 0);
+
+        return ans;
+    }
+}
