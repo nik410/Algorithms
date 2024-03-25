cc332227b773f78c6e426e45997bdbec424bef12
Niket Pandey <107863847+nik410@users.noreply.github.com>
2024-03-25 09:19:02 +0530

    Create Implement Queue using Arrays.java

---
+++ Queues/Implement Queue using Arrays.java
@@ -0,0 +1,27 @@
+public class Solution {
+    class Queue {
+        int front, rear;
+        int []arr;
+
+        Queue() {
+            front = 0;
+            rear = 0;
+            arr = new int[100001];
+        }
+
+        // Enqueue (add) element 'e' at the end of the queue.
+        public void enqueue(int e) {
+           arr[rear] = e;
+           rear++;
+        }
+
+        // Dequeue (retrieve) the element from the front of the queue.
+        public int dequeue() {
+            if(front == rear)return -1;
+            int ans = arr[front];
+            front++;
+            return ans;
+
+        }
+    }
+}
