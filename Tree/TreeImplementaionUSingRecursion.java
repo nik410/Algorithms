ca7595e407d825b59fa3680b9603e44ab418d0e7
Niket Pandey <107863847+nik410@users.noreply.github.com>
2024-04-06 23:19:14 +0530

    Create TreeImplementaionUsingRecursion.java

---
+++ Tree/TreeImplementaionUSingRecursion.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class TreeNode{
+	int data;
+	TreeNode left;
+	TreeNode right;
+	
+	public TreeNode(int data) {
+		// TODO Auto-generated constructor stub
+		this.data = data;
+		right = left = null;
+	}
+	
+}
+
+public class TreeImplementation {
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	public static TreeNode binaryTree() {
+		int x;
+		x = sc.nextInt();
+		if(x == -1)return null;
+		
+		TreeNode temp = new TreeNode(x);
+		System.out.println("Enter left child of " + temp.data +": ");
+		temp.left = binaryTree();
+		System.out.println("Enter right child of " + temp.data +": ");
+		temp.right = binaryTree();
+		
+		return temp;		
+	}
+
+	public static void main(String[] args) {
+		TreeNode rootNode;
+		System.out.println("Enter root Node: ");
+		rootNode = binaryTree();
+
+	}
+
+}
