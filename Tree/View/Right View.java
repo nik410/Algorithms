42e50a19e0be084757c22ac189a441efb9256bd3
Niket Pandey <107863847+nik410@users.noreply.github.com>
2024-04-09 18:30:57 +0530

    Update Right View.java

--- Tree/View/Right View.java
+++ Tree/View/Right View.java
@@ -1,76 +1,65 @@
 
-//User function Template for Java
-
-/* A Binary Tree node
+/*Complete The Function Provided
+Given Below is The Node Of Tree
 class Node
 {
     int data;
     Node left, right;
-
-    Node(int item)
+    public Node(int data)
     {
-        data = item;
-        left = right = null;
+        this.data = data;
+         left = right = null;
     }
 }*/
-class Tree
-{
-    void leftViewUsingRe(Node root, int level, ArrayList<Integer> ans){
+
+
+class Solution{
+    
+    
+    void rightViewUsingRec(Node root, int level, ArrayList<Integer> ans){
         if(root == null)return;
         
         if(level == ans.size())ans.add(root.data);
         
-        leftViewUsingRe(root.left, level + 1, ans);
-        leftViewUsingRe(root.right, level + 1, ans);
+        rightViewUsingRec(root.right, level + 1, ans);
+        rightViewUsingRec(root.left, level + 1, ans);
+    }
+    
+    
+    //Function to return list containing elements of right view of binary tree.
+    ArrayList<Integer> rightView(Node node) {
+        
+        ArrayList<Integer> ans = new ArrayList();
+        
+        rightViewUsingRec(node, 0, ans);
+        
+        return ans;
+        // Queue<Node> q = new LinkedList();
+        
+        // q.offer(node);
+        
+        
+        // while(!q.isEmpty()){
+        //     int siz = q.size();
+        //     int right = -1;
+        //     for(int i = 0; i < siz; i++){
+        //         Node temp = q.peek();
+        //         right = temp.data;
+        //         if(temp.left != null){
+        //             q.offer(temp.left);
+        //         }
+                
+        //         if(temp.right != null){
+        //             q.offer(temp.right);
+        //         }
+                
+        //         q.poll();
+        //     }
+        //     ans.add(right);
+        // }
+        
+        // return ans;
         
     }
-    //Function to return list containing elements of left view of binary tree.
-    ArrayList<Integer> leftView(Node root)
-    {
-      ArrayList<Integer> ans = new ArrayList();
-      leftViewUsingRe(root, 0, ans);
-      return ans;
-      
-      
-    //   if(root == null)return ans;
-    //   Queue<Node> q = new LinkedList();
-      
-    //   q.offer(root);
-    //   //ans.add(root.data);
-      
-    //   while(!q.isEmpty()){
-    //       int siz = q.size();
-          
-    //       //int leftV =-1, flag = 0;
-    //       ans.add(q.peek().data);
-          
-    //       for(int i = 0; i < siz; i++){
-    //           Node temp = q.peek();
-              
-    //           if(temp.left != null){
-    //               q.add(temp.left);
-    //             //   if(flag == 0){
-    //             //       leftV = temp.left.data;
-    //             //       flag = 1;
-    //             //   }
-    //           }
-              
-    //           if(temp.right != null){
-    //               q.add(temp.right);
-    //             //   if(flag == 0){
-    //             //       leftV = temp.right.data;
-    //             //       flag = 1;
-    //             //   }
-    //           }
-    //           q.poll();
-    //       }
-          
-    //       //if(leftV != -1)
-         
-    //   }
-      
-      
-    //   return ans;
-      
-    }
 }
+
