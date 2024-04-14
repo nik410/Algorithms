a8852ec02029437cbaff1c73684dbdad74ab1f70
Niket Pandey <107863847+nik410@users.noreply.github.com>
2024-04-14 18:00:45 +0530

    Update Inorder.java

--- Tree/Traversals/Morris Traversal/Inorder.java
+++ Tree/Traversals/Morris Traversal/Inorder.java
@@ -1,1 +1,60 @@
-
+class Solution
+{
+    // Return a list containing the inorder traversal of the given tree
+    ArrayList<Integer> inOrder(Node root)
+    {
+        ArrayList<Integer> al = new ArrayList();
+        // Stack<Node> st = new Stack();
+        
+        // Node n = root;
+        
+        // while(true){
+        //     if(n != null){
+        //         st.push(n);
+        //         n = n.left;
+        //     }
+        //     else{
+        //         if(st.isEmpty())break;
+        //         n = st.pop();
+        //         al.add(n.data);
+        //         n = n.right;
+        //     }
+        // }
+        
+        // return al;
+        
+        
+        while(root != null){
+            if(root.left == null){
+                al.add(root.data);
+                root = root.right;
+            }
+            else{
+                
+                Node temp = root.left;
+                while(temp.right != null && temp.right != root){
+                    temp = temp.right;
+                }
+                //Left Subtree not traversed or not
+                if(temp.right == null){
+                    temp.right = root;
+                    root = root.left;
+                }
+                    
+                else{
+                        temp.right = null;
+                        al.add(root.data);
+                        root = root.right;
+                }
+            }
+            
+        }
+        
+        return al;
+        
+        
+        
+    }
+    
+    
+}
